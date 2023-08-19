f895b34daec251e7746a2aa5efdf329d06b96e48
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2023-08-19 19:29:13 +0300

    fixed routes problem for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
+  devise_for :users, path: 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
