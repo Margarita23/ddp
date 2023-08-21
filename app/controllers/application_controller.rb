fb484095417007f0e48f5c6d5497ae0de4a1fe3f
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2023-08-21 22:36:29 +0300

    add some settings for users outh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || board_index_path
