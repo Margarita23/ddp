581054d21578c81a9cfb9a914d578b8b96573be2
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2023-08-22 15:38:32 +0300

    add some styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: :switch_locale
 
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || board_index_path
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     devise_for :users, path: 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    get "board/index"
+    get 'board/index'
 
     get 'locale/:lang', action: :switch_locale, controller: 'application', as: 'locale'
 
